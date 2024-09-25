94c17fa60ad347ae640b4590dc9c3b0f59839624
Leko1705 <lennart1705@gmail.com>
2024-09-25 23:16:41 +0200

    - added some simple common icons in UI

--- src/main/java/proman/filesys/Resources.java
+++ src/main/java/proman/filesys/Resources.java
@@ -1,15 +1,44 @@
 package proman.filesys;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Resources {
 
     public static Icon loadResourceIcon(String name) {
         String location = FileSystem.getInstallationPath();
+        location += File.separator + "images";
 
-        // TODO load Icon from resources relative to installation path
+        File imageDir = new File(location);
+        imageDir.mkdirs();
 
-        return null;
+        try {
+            File imageFile = new File(imageDir, name);
+            return new ImageIcon(ImageIO.read(imageFile));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public static Icon loadIcon(String path) {
+        File imageFile = new File(path);
+
+        if (!imageFile.exists())
+            return null;
+
+        if (imageFile.isDirectory())
+            return null;
+
+        try {
+            return new ImageIcon(ImageIO.read(imageFile));
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
